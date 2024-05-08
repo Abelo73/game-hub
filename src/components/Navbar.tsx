0f4561ff6a5aa844f1b5f5b879ed5f81f1d5d8b3
Abel Adisu <abeladisu73@gmail.com>
2024-05-08 09:50:51 +0300

    Build a color mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,13 @@
 import { HStack, Image, Text } from '@chakra-ui/react'
 
 import logo from '../assets/logo.webp'
+import ColorModeSwitch from './ColorModeSwitch'
 
 const Navbar = () => {
   return (
-    <HStack>
+    <HStack justifyContent='space-between' padding='10px'>
         <Image src={logo}  boxSize="60px"/>
-        <Text fontSize="2xl">Game Hub</Text>
+        <ColorModeSwitch />
     </HStack>
   )
 }
