884e4246a7c5fa8f90b851aa695b06cffcc5e945
Abel Adisu <abeladisu73@gmail.com>
2024-05-08 11:26:48 +0300

    Displaying Platform Icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,13 @@
-import React from 'react'
+
+import {FaLinux, FaWindows, FaXbox, FaPlaystation, FaAndroid, FaApple} from 'react-icons/fa'
+
+import {MdPhoneIphone} from 'react-icons/md'
+
+import {SiNintendo} from 'react-icons/si'
+import {BsGlobe} from 'react-icons/bs'
 import { Game } from '../hooks/useGames'
-import { Card, CardBody, Heading, Image } from '@chakra-ui/react'
+import { Card, CardBody, Heading, Image, Text } from '@chakra-ui/react'
+import PlatformIconList from './PlatformIconList'
 
 interface Props{
     game:Game
@@ -12,6 +19,7 @@ const GameCard = ({game}:Props) => {
         <Image src={game.background_image} />
         <CardBody>
             <Heading fontSize='2xl'>{game.name}</Heading>
+            <PlatformIconList platforms={game.parent_platforms.map((p) => p.platform)} />
         </CardBody>
     </Card>
   )
