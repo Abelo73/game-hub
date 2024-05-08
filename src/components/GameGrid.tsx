e58cf6d8c95d161f77e9fc31aa42cef264d61ba2
Abel Adisu <abeladisu73@gmail.com>
2024-05-08 14:21:37 +0300

    Create a generic data fetching hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,29 +4,33 @@ import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 
-
-
 const GameGrid = () => {
-    const {games, error, isLoading}=useGames();
+  const { data, error, isLoading } = useGames();
 
-    const skeletons = [1,2,3,4,5,6];
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
-  
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{
-        sm:1, md:2, lg:3, xl:5
-      }} spacing={10}>
-        {isLoading && skeletons.map((skeleton) => (
-           <GameCardContainer>
-             <GameCardSkeleton key={skeleton}/>
-           </GameCardContainer>
-        ))}
-        {games.map((game) => (
-           <GameCardContainer>
-             <GameCard key={game.id} game={game}/>
-           </GameCardContainer>
+      <SimpleGrid
+        columns={{
+          sm: 1,
+          md: 2,
+          lg: 3,
+          xl: 5,
+        }}
+        spacing={10}
+      >
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer>
+              <GameCardSkeleton key={skeleton} />
+            </GameCardContainer>
+          ))}
+        {data.map((game) => (
+          <GameCardContainer>
+            <GameCard key={game.id} game={game} />
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
+import useData from "./useDate";
 
 export interface Platform{
     id:number;
@@ -13,43 +14,12 @@ export interface Game {
   id: number;
   name: string;
   background_image:string;
-
   parent_platforms:{platform:Platform}[];
   metacritic:number
 
 }
 
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
 
-const useGames = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false)
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    setIsLoading(true);
-
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => {
-        setGames(res.data.results)
-        setIsLoading(false) 
-    })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setIsLoading(false)
-      });
-
-    return () => controller.abort();
-  }, []);
-
-  return { games, error, isLoading };
-};
+const useGames = () => useData<Game[]>('/games')
 
 export default useGames;
