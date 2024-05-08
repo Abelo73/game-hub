35b791428918998fff521b796a5596aece5751a1
Abel Adisu <abeladisu73@gmail.com>
2024-05-08 15:16:43 +0300

    Highligth Selected Genre;

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5} >
-          <GeneraList onSelectGenre={(genre)=>setSelectedGenre(genre)}/>
+          <GeneraList selectedGenre={selectedGenre} onSelectGenre={(genre)=>setSelectedGenre(genre)}/>
         </GridItem>
       </Show>
       <GridItem area="main">
--- src/components/GeneraList.tsx
+++ src/components/GeneraList.tsx
@@ -6,9 +6,10 @@ import useGenres, { Genre } from '../hooks/useGenres'
 
 interface Props{
     onSelectGenre:(genre:Genre)=>void;
+    selectedGenre: Genre | null
 }
 
-const GeneraList = ({onSelectGenre}:Props) => {
+const GeneraList = ({selectedGenre,onSelectGenre}:Props) => {
     const {data, isLoading, error} = useData<Genre>('/genres')
 
     if (error) return null
@@ -20,7 +21,7 @@ const GeneraList = ({onSelectGenre}:Props) => {
     {data.map((genre) => <ListItem paddingY='5px'  key={genre.id}>
         <HStack>
             <Image boxSize='32px' borderRadius={8} src={genre.image_background}/>
-            <Button onClick={()=>onSelectGenre(genre)} variant='link' fontSize='lg'>{genre.name}</Button>
+            <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}  onClick={()=>onSelectGenre(genre)} variant='link' fontSize='lg'>{genre.name}</Button>
         </HStack>
     </ListItem>)}
    </List>
