dc01530a7033def7cc47100651ce88631d1c2440
Abel Adisu <abeladisu73@gmail.com>
2024-05-09 14:21:57 +0300

    Clean up the genres

--- src/components/GeneraList.tsx
+++ src/components/GeneraList.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react'
+import { Button, HStack, Heading, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react'
 import useData from '../hooks/useData'
 import useGenres, { Genre } from '../hooks/useGenres'
 
@@ -17,14 +17,19 @@ const GeneraList = ({selectedGenre,onSelectGenre}:Props) => {
     if (isLoading) return<Spinner  />
   
     return (
+
+
+   <>
+   <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
    <List>
     {data.map((genre) => <ListItem paddingY='5px'  key={genre.id}>
         <HStack>
-            <Image boxSize='32px' borderRadius={8} src={genre.image_background}/>
-            <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}  onClick={()=>onSelectGenre(genre)} variant='link' fontSize='lg'>{genre.name}</Button>
+            <Image boxSize='32px' borderRadius={8} objectFit='cover' src={genre.image_background}/>
+            <Button whiteSpace='normal' textAlign='left' fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}  onClick={()=>onSelectGenre(genre)} variant='link' fontSize='lg'>{genre.name}</Button>
         </HStack>
     </ListItem>)}
    </List>
+   </>
   )
 }
 
