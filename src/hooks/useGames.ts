e3ea60b8ee88f2423b0aa2d79d9afadc9bcaee8c
Abel Adisu <abeladisu73@gmail.com>
2024-05-09 11:14:49 +0300

    refactoring extract query element

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,6 +3,7 @@ import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 import useData from "./useData";
 import { Genre } from "./useGenres";
+import GameQuery from "../App";
 
 export interface Platform {
   id: number;
@@ -19,13 +20,12 @@ export interface Game {
 }
 
 const useGames = (
-  selectedGenre: Genre | null,
-  selectedPlatform: Platform | null
+  gameQuery:GameQuery
 ) =>
   useData<Game[]>(
     "/games",
-    { params: { genres: selectedGenre?.id, platforms: selectedPlatform?.id } },
-    [selectedGenre?.id, selectedPlatform?.id]
+    { params: { genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id } },
+    [gameQuery]
   );
 
 export default useGames;
